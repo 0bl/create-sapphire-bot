df9cb070957ab9f9ca7d46c1bc0821901425e49c
Antonio Román <kyradiscord@gmail.com>
2021-05-08 01:56:06 +0200

    Initialize build

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.main = void 0;
+const main = () => 'this builds and pushes';
+exports.main = main;
+exports.default = exports.main;
+//# sourceMappingURL=index.js.map
